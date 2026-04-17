8a9967aec810b56f6eca51f11213271c776f2eac
MD <105647956+Dhruvmer610@users.noreply.github.com>
2026-04-17 10:17:40 +0530

    Update MongoDB URL to use environment variable
    Remove hardcoded MongoDB connection string.

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const MONGO_URL = process.env.MONGO_URL || "mongodb+srv://dhruvmer610_db_user:0DXFXaFIDKzOI8nL@cluster0.vmf9lyk.mongodb.net/tea-tracker";
+const MONGO_URL = process.env.MONGO_URL
 
 app.use(cors());
 app.use(express.json());
